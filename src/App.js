5aad795c18861edff3c070d645ca912bcbdb563c
Brannon Wadforth <whiskey@digitalnode.io>
2020-08-19 14:44:05 +1000

    Updating styles

--- src/App.js
+++ src/App.js
@@ -10,6 +10,27 @@ export const ROUTES = {
   PROJECTS: '/projects',
 };
 
+function Alert({ status, title, description }) {
+  return (
+    <div className={`alert ${status ? `alert-${status}` : `alert-info`}`}>
+      <h1>{title}</h1>
+      {description && <p>{description}</p>}
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <Alert
+        status="warning"
+        title="Not Found"
+        description="The page you accessed does not exist yet or never, ever existed."
+      />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -17,10 +38,7 @@ function App() {
         <main>
           <Switch>
             <Route exact path={ROUTES.HOME} component={Dashboard} />
-            <Route exact path={ROUTES.ARTICLES} component={Articles} />
-            {/*
-           
-            <Route exact path={ROUTES.PROJECTS} component={Articles}/>*/}
+            <Route exact component={NotFound} />
           </Switch>
         </main>
         {/*<Navigation />*/}
