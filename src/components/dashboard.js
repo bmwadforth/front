8c70e69d0635e4c02201294a692ef1c608762d9b
Brannon Wadforth <whiskey@digitalnode.io>
2020-08-24 19:27:27 +1000

    Updating steps in dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -11,13 +11,12 @@ export default function Dashboard() {
       <div className="dashboard">
         <div className="oh-no">
           {step === 0 && <h1 onClick={() => setStep(1)}>Click me</h1>}
-          {step === 1 && (
-            <h1 onClick={() => setStep(2)}>Never, ever click something when someone asks you to. Click me again.</h1>
-          )}
-          {step === 2 && <h1 onClick={() => setStep(3)}>.........</h1>}
-          {step === 3 && <h1 onClick={() => setStep(4)}>Don't.</h1>}
-          {step === 4 && <h1 onClick={() => setStep(5)}>Stop.</h1>}
-          {step === 5 &&
+          {step === 1 && <h1 onClick={() => setStep(2)}>Again.</h1>}
+          {step === 2 && <h1 onClick={() => setStep(3)}>One more time.</h1>}
+          {step === 3 && <h1 onClick={() => setStep(4)}>Ok no more.</h1>}
+          {step === 4 && <h1 onClick={() => setStep(5)}>Don't.</h1>}
+          {step === 5 && <h1 onClick={() => setStep(6)}>Stop.</h1>}
+          {step === 6 &&
             window.location.replace('https://www.cyber.gov.au/acsc/view-all-content/programs/stay-smart-online')}
         </div>
       </div>
