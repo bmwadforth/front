6004b25e38e69c22102d891a9b4f7162df9168e0
Brannon Wadforth <whiskey@digitalnode.io>
2020-08-27 16:35:47 +1000

    Updating theme

--- src/components/common/codeBlock.js
+++ src/components/common/codeBlock.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { anOldHope } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { androidstudio } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function CodeBlock(props) {
   return (
-    <SyntaxHighlighter language={props.language} style={anOldHope}>
+    <SyntaxHighlighter language={props.language} style={androidstudio}>
       {props.value}
     </SyntaxHighlighter>
   );
