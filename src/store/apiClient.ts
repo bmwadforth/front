c0074def5c0580ddb51057989594beace4959024
Brannon Wadforth <whiskey@digitalnode.io>
2022-09-16 23:01:40 +1000

    updating

--- src/store/apiClient.ts
+++ src/store/apiClient.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const axiosClient = axios.create();
 
-axiosClient.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost";
+axiosClient.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 export default axiosClient;
